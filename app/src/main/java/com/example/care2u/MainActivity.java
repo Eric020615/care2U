f0f5dc8f193352d9185d158749aa1d0af85cc59c
ericj <ericjuncheng10@gmail.com>
2022-12-03 00:10:58 +0800

    For Base

--- app/src/main/java/com/example/care2u/MainActivity.java
+++ app/src/main/java/com/example/care2u/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.care2u;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import com.example.care2u.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.bottomNavView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home_button:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.schedule_button:
+                    replaceFragment(new ScheduleFragment());
+                    break;
+                case R.id.chat_button:
+                    replaceFragment(new ChatFragment());
+                    break;
+                case R.id.notification_button:
+                    replaceFragment(new NotificationFragment());
+                    break;
+                case R.id.profile_button:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }

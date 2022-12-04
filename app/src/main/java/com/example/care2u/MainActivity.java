bb481cb04fb653958853f9e001b7d5b87483fc35
ericj <ericjuncheng10@gmail.com>
2022-12-04 21:40:16 +0800

    .

--- app/src/main/java/com/example/care2u/MainActivity.java
+++ app/src/main/java/com/example/care2u/MainActivity.java
@@ -7,37 +7,55 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.example.care2u.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
-    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setSupportActionBar(findViewById(R.id.tool_app));
+        getSupportActionBar().setTitle("Home");
         replaceFragment(new HomeFragment());
+
+        binding.toolApp.setOnMenuItemClickListener(item -> {
+            switch(item.getItemId()){
+                case R.id.logout_button:
+                    Intent intent = new Intent(MainActivity.this,StartingActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+            return true;
+        });
 
         binding.bottomNavView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()){
                 case R.id.home_button:
+                    getSupportActionBar().setTitle("Home");
                     replaceFragment(new HomeFragment());
                     break;
                 case R.id.schedule_button:
+                    getSupportActionBar().setTitle("Schedule");
                     replaceFragment(new ScheduleFragment());
                     break;
                 case R.id.chat_button:
+                    getSupportActionBar().setTitle("Chat");
                     replaceFragment(new ChatFragment());
                     break;
                 case R.id.notification_button:
+                    getSupportActionBar().setTitle("Notification");
                     replaceFragment(new NotificationFragment());
                     break;
                 case R.id.profile_button:
+                    getSupportActionBar().setTitle("Profile");
                     replaceFragment(new ProfileFragment());
                     break;
             }
@@ -46,10 +64,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.fragment_container, fragment);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_top,menu);
+        return true;
     }
 }

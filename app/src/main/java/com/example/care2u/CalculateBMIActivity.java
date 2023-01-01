06132c918c8b9f4f8346da9ce13d6d9da7997dbf
HO KAI CHONG <feihaidao77@gmail.com>
2023-01-01 18:25:41 +0800

    bmi

---
+++ app/src/main/java/com/example/care2u/CalculateBMIActivity.java
@@ -0,0 +1,72 @@
+package com.example.care2u;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DecimalFormat;
+
+public class CalculateBMIActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText height_et;
+    private EditText weight_et;
+    private TextView result_tv;
+    private TextView status_tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_bmiactivity);
+
+        ImageView close = findViewById(R.id.close_iv);
+        height_et = findViewById(R.id.height_ET);
+        weight_et = findViewById(R.id.weight_ET);
+        Button calculate_btn = findViewById(R.id.calculate_BMI_button);
+        result_tv = findViewById(R.id.result_tv);
+        status_tv = findViewById(R.id.status_tv);
+
+        calculate_btn.setOnClickListener(this);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        Double height = Double.parseDouble(height_et.getText().toString());
+        Double weight = Double.parseDouble(weight_et.getText().toString());
+
+        double bmi = weight / ((height/100) * (height/100));
+        String status ="";
+        if (bmi>=30.0){
+            status = "Obesity";
+        }else if(bmi >=25.0 && bmi <=29.9){
+            status = "Overweight";
+        }else if(bmi >=18.5 && bmi <=24.9){
+            status = "Healthy Weight";
+        }else if(bmi <=18.5){
+            status = "Underweight";
+        }
+        DecimalFormat df = new DecimalFormat("#.#");
+        result_tv.setText(String.valueOf(df.format(bmi)));
+        status_tv.setText(status);
+
+        height_et.setText("");
+        weight_et.setText("");
+    }
+}
+
+//    BMI	Weight Status
+//        Below 18.5	Underweight
+//        18.5—24.9	Healthy Weight
+//        25.0—29.9	Overweight
+//        30.0 and Above	Obesity
--- app/src/main/java/com/example/care2u/HealthRecordActivity.java
+++ app/src/main/java/com/example/care2u/HealthRecordActivity.java
@@ -1,7 +1,9 @@
 package com.example.care2u;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,15 +15,35 @@ public class HealthRecordActivity extends AppCompatActivity implements View.OnCl
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_record);
         ImageView back_button = findViewById(R.id.IV_back);
+        Button calculate_BMI_button = findViewById(R.id.calculate_BMI_button);
+        Button depression_test_button = findViewById(R.id.depression_test_button);
+        Button consultation_history_button = findViewById(R.id.consultation_history_button);
+        Button reference_button = findViewById(R.id.reference_button);
         back_button.setOnClickListener(this);
+        calculate_BMI_button.setOnClickListener(this);
+        depression_test_button.setOnClickListener(this);
+        consultation_history_button.setOnClickListener(this);
+        reference_button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        Intent intent;
         switch (view.getId()){
             case R.id.IV_back:
                 finish();
                 break;
+            case R.id.calculate_BMI_button:
+                intent = new Intent(HealthRecordActivity.this,CalculateBMIActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.depression_test_button:
+                break;
+            case R.id.consultation_history_button:
+                break;
+            case R.id.reference_button:
+                break;
+
         }
     }
 }
